6a814a852ba3cc14d8ca764a756965d10785cdf7
alex <sowillmove1@gmail.com>
2024-05-24 19:01:30 +0300

    Service distribution

---
+++ ui-service/src/main/java/ru/spbstu/metricsui/MetricsUiApplication.java
@@ -0,0 +1,11 @@
+package ru.spbstu.metricsui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsUiApplication.class, args);
+    }
+}
